7465d86a4f420f91db8b4c90c7fc4fb7294a0e2a
Anton Mishkevich <mian19@protonmail.com>
2022-11-28 22:04:06 +0300

    init: start ImageSearcher project

---
+++ ImageSearcher/ImageSearcher/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  ImageSearcher
+//
+//  Created by Kyzu on 28.11.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
---
+++ ImageSearcher/ImageSearcher/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageSearcher
+//
+//  Created by Kyzu on 28.11.22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
