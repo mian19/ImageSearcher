b688b35924acd94e6ab7e8d7a1d499462d807b4e
Anton Mishkevich <mian19@protonmail.com>
2022-12-01 01:43:32 +0300

    feat: add enum for alerts

---
+++ ImageSearcher/ImageSearcher/Support/AlertMessage.swift
@@ -0,0 +1,15 @@
+//
+//  AlertMessage.swift
+//  ImageSearcher
+//
+//  Created by Kyzu on 30.11.22.
+//
+
+import Foundation
+
+enum AlertMessage: String {
+    case ok = "ok"
+    case alredyTag = "This tag alredy added"
+    case emptyTag = "Tag can't be empty"
+    case emptyResult = "Try to use another tags"
+}
